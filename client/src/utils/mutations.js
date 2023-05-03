fc5042b2eccc1e8bd423823379a8aecb5e0e204a
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-05-03 19:07:08 -0400

    added mutations, refactored components and pages

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client'
+
+//login
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!){
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+//add user
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser(username: $username, email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }
+`
+
+//save book
+export const SAVE_BOOK = gql`
+    mutation saveBook($userId: ID!, $book: BookInput!){
+        saveBook(userId: $userId, book: $book){
+            _id
+            username
+            savedBooks{
+                bookId
+            }
+        }
+    }
+`
+
+//remove book
+export const REMOVE_BOOK = gql`
+mutation removeBook($userId: ID!, $book: BookInput!){
+  removeBook(userId: $userId, book: $book){
+    _id
+    username
+    email
+    bookCount
+    savedBooks {
+      bookId
+      authors
+      description
+      title
+      image
+      link
+    }
+  }
+}
+`
