6e48c49876f90aabf9819a327ab85f3486b9f60a
AnthonyJohnson101 <tonybologna123@outlook.com>
2023-05-03 23:59:46 -0400

    bug fix

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,7 +5,7 @@ const { signToken } = require('../utils/auth');
 
 //Define resolvers
 const resolvers = {
-    //Our "getUser" query that finds the current user
+    //finds the current user
     Query: {
         getUser: async (parent, { userId }) => {
             return User.findOne({ _id: userId })
@@ -13,7 +13,7 @@ const resolvers = {
     },
 
     Mutation: {
-        //Our "login" mutation that finds our user, and then compares passwords, and then returns the user and the jwt token
+        //compares passwords, returns the user and the jwt token
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
 
@@ -26,15 +26,14 @@ const resolvers = {
             const token = signToken(user);
             return { token, user }
         },
-        //Our "adduser" mutation that simply adds a user to our database and returns that user with a jwt token 
+        //adds a user to our database and returns with a jwt token 
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password })
             const token = signToken(user)
 
             return { token, user };
         },
-        //Our "savebook" mutation that finds the user in question (in our case, the current user),
-        //and then adds the book in question to the user's saveBooks field
+        //finds the user, adds the book to saveBooks 
         saveBook: async (parent, { userId, book }) => {
             const user = await User.findOneAndUpdate(
                 { _id: userId },
@@ -48,8 +47,7 @@ const resolvers = {
             )
             return user
         },
-        //Our "removebook" mutation that finds the user in question and then
-        //removes the book in question from the user's saveBooks field
+        //finds the user in question and then removes the book
         removeBook: async (parent, { userId, book }) => {
             const user = await User.findOneAndUpdate(
                 { _id: userId },
